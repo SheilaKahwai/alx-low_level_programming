567de9776e0ee4d5fa7f80bca6aa78664a0969d9
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-02 11:21:49 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
+/*
+ *main prints to screen
+ */
+
 #include <stdio.h>
 
 int main(void)
 {
-char str1[] = "Programming is like building a multilingual puzzle";
+char str1[] = ""Programming is like building a multilingual puzzle";
 puts(str1);
 return (0);
 }
