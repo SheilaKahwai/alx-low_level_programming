f63374f39f6db0fc7893a2dc86969ef495a4f0b0
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-02 22:52:39 -0700

    print to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints to stderr
+ * Return: 1 on successful exit
+ */
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 60);
+return (1);
+}
