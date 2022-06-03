e59c8750d5e27df45a9e2da6bf64c25693fe3226
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-03 12:46:24 -0700

    print combination of numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints combinations of two digits
+ * Return: 0 on successful exit
+ */
+
+int main(void)
+{
+int x = 0;
+while (x < 10)
+{
+int y = 1;
+while (y < 10)
+{
+if (x != y && x < y)
+{
+putchar(x + '0');
+putchar(y + '0');
+if (x != 8 || y !=9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+y++;
+}
+x++;
+}
+putchar('\n');
+return (0);
+}
