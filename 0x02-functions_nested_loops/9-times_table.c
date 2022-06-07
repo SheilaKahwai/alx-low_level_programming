a2e6c99375af5b510ea30f1c2f50a304afc26d59
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-07 08:21:55 -0700

    print times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table
+ */
+
+void times_table(void)
+{
+int prod;
+int x = 0;
+while (x < 10)
+{
+int y = 0;
+while (y < 10)
+{
+prod = x * y;
+if (y == 0)
+_putchar(x + '0');
+if (prod < 10 && y != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(prod + '0');
+}
+else if (prod >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((prod / 10) + '0');
+_putchar((prod % 10) + '0');
+}
+y++;
+}
+x++;
+}
+}
