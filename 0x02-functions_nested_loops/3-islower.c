1acef08120f9edf735879420093db9c2393641f5
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-07 04:39:12 -0700

    checks if letter is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,3 +1,9 @@
+/**
+ * _islower - checks if letter is lowercase
+ * @c: character to check
+ * Return: 1 on lowercase, 0 otherwise
+ */
+
 int _islower(int c)
 {
 int res;
@@ -5,5 +11,5 @@ if (c >= 97 && c <= 122)
 res = 1;
 else
 res = 0;
-return res;
+return (res);
 }
