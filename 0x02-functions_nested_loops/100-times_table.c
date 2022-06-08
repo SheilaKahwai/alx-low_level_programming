76cb79038e5b576b83475be32b51e608d3e71cba
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-08 05:50:19 -0700

    print times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,45 +6,35 @@
 void print_times_table(int n)
 {
 int prod;
-int x = 0;
-while (x <= n)
-{
-int y = 0;
-while (y <= n)
-{
-if (n >= 0 && n <= 15)
-{
+int x, y;
+if (n > 0 && n < 15){
+for (x = 0; x < n; x++){
+for (y = 0; y < n; y++){
 prod = x * y;
 if (y == 0)
 _putchar(prod + '0');
-else if (prod < 10 && y != 0)
-{
+else if (prod < 10 && y != 0){
 _putchar(',');
-_putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(prod + '0');
 }
-else if (prod >= 10 && prod < 100)
-{
+else if (prod >= 10){
 _putchar(',');
-_putchar(' ');
 _putchar(' ');
 _putchar((prod / 10) + '0');
 _putchar((prod % 10) + '0');
 }
-else if (prod >= 100)
-{
+else if (prod > 100){
 _putchar(',');
-_putchar(' ');
-_putchar((prod / 100) + '0');
-_putchar(((prod / 10) % 10) + '0');
+_putchar((prod / 10) + '0');
+_putchar((prod / 10) + '0');
 _putchar((prod % 10) + '0');
-}
 }
 y++;
 }
-x++;
 _putchar('\n');
+x++;
+}
 }
 }
