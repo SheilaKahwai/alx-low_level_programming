4fcd80cfdc4919fa5a4046b4b42105a1f54494a9
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-08 07:18:46 -0700

    prints fibonacci series

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,12 +13,19 @@ long int y = 2;
 long int sum = 0;
 printf("%ld, ", x);
 printf("%ld, ", y);
-for (c = 0; c < 50; c++)
+for (c = 1; c <= 50; c++)
 {
 sum = x + y;
+if (c != 50)
+{
 printf("%ld, ", sum);
 x = y;
 y = sum;
+}
+else
+{
+printf("%ld", sum);
+}
 }
 printf("\n");
 return (0);
