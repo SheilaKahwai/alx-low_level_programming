36476018e74a0835d3453d94b181a80f7fc7ebbe
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-08 06:17:24 -0700

    print times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,7 +30,7 @@ _putchar((prod % 10) + '0');
 else if (prod >= 100){
 _putchar(',');
  _putchar(' ');
-_putchar((prod / 10) + '0');
+_putchar((prod / 100) + '0');
 _putchar(((prod / 10) % 10) + '0');
 _putchar((prod % 10) + '0');
 }
