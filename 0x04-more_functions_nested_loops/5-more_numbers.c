18b02e822a5bce856ba17ad6e06169ff402eff85
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-09 03:31:40 -0700

    prints numbers 0-14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,13 +12,9 @@ for  (x = 0; x < 10; x++)
 {
 for (y = 0; y < 15; y++)
 {
-if (y < 10)
-_putchar(y + '0');
-else if (y >= 10)
-{
+if (y >= 10)
 _putchar((y / 10) + '0');
 _putchar((y % 10) + '0');
-}
 }
 _putchar('\n');
 }
