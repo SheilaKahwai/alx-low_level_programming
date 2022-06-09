85b0e8de775c76561bf71321d8b38eef3fa74c7c
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-09 10:07:51 -0700

    print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,15 @@
 
 void print_number(int n)
 {
+int x;
 if (n < 0)
 {
 _putchar('-');
-n = -n;
+x = -n;
 }
-if (n / 10)
-print_number(n / 10);
-_putchar((n % 10) + '0');
+else
+x = n;
+if (x / 10)
+print_number(x / 10);
+_putchar((x % 10) + '0');
 }
