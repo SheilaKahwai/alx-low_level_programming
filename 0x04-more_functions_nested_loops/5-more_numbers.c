c824a2f613815db8bad35f24d1b04edcace29832
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-09 03:25:28 -0700

    prints numbers 0-14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,10 +14,12 @@ for (y = 0; y < 15; y++)
 {
 if (y < 10)
 _putchar(y + '0');
-else
-_putchar(((y / 10) + '0')((y % 10) + '0'));
+else if
+{
+_putchar((y / 10) + '0');
+_putchar((y % 10) + '0');
+}
 }
 _putchar('\n');
 }
 }
-	  
