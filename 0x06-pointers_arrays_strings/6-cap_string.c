3ca6f3c2b6d8e1de8d70e8dd9df1d71dc7fbdbd9
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-16 03:07:14 -0700

    update code

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,17 +6,21 @@
 
 char *cap_string(char *str)
 {
-	int i;
+	int i, j;
+
+	char spec[] = {' ', ',', ';', '.', '!', '?', '"',
+		'(', ')', '{', '}', '\n', '\t'};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 32 && str[i] <= 44 || str[i] == 46 || str[i] == 47 || str[i] == '\n' || str[i] == '\t' || str[i] == 123 || str[i] == 125 || str[i] == 63 || str[i] == 59)
-		{ 
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+		for (j = 0; spec[j] != '\0'; j++)
+		{
+			if (str[i] == spec[j])
 			{
-				str[i + 1] = str[i + 1] - 32;
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+					str[i + 1] -= 32;
 			}
 		}
-
+	}
 	return (str);
 }
