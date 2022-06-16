605cb6e836bad88ab9ab641461b0a4509ae52b0b
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-16 11:45:05 -0700

    password generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - generates random passwords
+ * Return: 0 on successful exit
+ */
+
+int main(void)
+{
+int i, x;
+char pass[100];
+
+char list[] = "abcdefghijklmnopqrstuvwxyzABCDEFG" +
+	"HIJKLMNOPQRSTUVWXYZ0123456789~`!@#$^&*()_-+={}[]|<,>.?/";
+
+srand(time(NULL));
+for (i = 0; i < 100; i++)
+{
+	x = rand() % 88;
+	pass[i] = list[x];
+}
+for (i = 0; i < 100; i++)
+{
+printf("%s ", pass);
+}
+
+return (0);
+}
