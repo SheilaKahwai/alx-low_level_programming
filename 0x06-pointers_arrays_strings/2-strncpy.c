a7e4426dfba198c6185b670439bab5753b6c4469
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-16 06:04:51 -0700

    copy n characters of string to dest

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,26 @@
+/**
+ * _strncpy - copies string from src to dest
+ * @dest: destination string
+ * @src: source string
+ * @n: number of characters of string src to copy
+ * Return: pointer to resulting string
+ */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
