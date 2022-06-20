d3ee8c3e25547895cdf708c637b142630187f74e
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-20 04:47:25 -0700

    update code

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include<string.h>
 
 /**
  * _strpbrk - searches a string for any of a set of bytes
@@ -9,16 +9,18 @@
 
 char *_strpbrk(char *s, char *accept)
 {
+	int i, j;
+
 	if ((s == NULL) && (accept == NULL))
 		return (NULL);
 
-	while (*s)
+	for (i = 0; s[i] != '\0'; s++)
 	{
-		if _strchr(accept, s)
-			return (s);
-		else
-			s++;
-	}
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s);
+		}
 
 	return (NULL);
 }
