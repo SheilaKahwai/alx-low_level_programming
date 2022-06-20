db2da82994632fcef945c8eea20359600834e27e
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-20 01:42:48 -0700

    search for character in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+/**
+ * _strchr - searches for the first occurrence of a 
+ * character in a string
+ * @s: pointer to string to search
+ * @c: character to search for in the string
+ * Return: string or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *flag = NULL;
+
+	if (s != NULL)
+	{
+		do
+		{
+			if(*s == c)
+			{
+				flag = s;
+				break;
+			}
+		}
+		while (*s++);
+	}
+
+	return (flag);
+}
