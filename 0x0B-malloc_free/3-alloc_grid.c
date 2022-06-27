bd01b7a93ae29a29997123b7baf04ff5897b4518
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-27 04:35:41 -0700

    print a 2d array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	ar = (int *)malloc((width * height) * sizeof(int));
+	ar = (int **)malloc((width * height) * sizeof(int));
 	for (i = 0; i < width; i++)
 	{
 		for (j = 0; j < height; j++)
