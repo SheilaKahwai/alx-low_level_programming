2ea345ec34011ed5bb05e4d54410c622081e934c
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-27 07:14:40 -0700

    free 2D grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: 2d array
+ * @height: no. of colums of array
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height == 0)
+		return;
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
