f3b5bc4bd1fac02a2070fd0a1ec74439c875efc8
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-27 00:20:47 -0700

    create an array and initialize it with char c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	ar = malloc(size * sizeof(char));
+	ar = (char *)malloc(size * sizeof(char));
 	if (ar == NULL)
 		return (NULL);
 	ar[0] = c;
