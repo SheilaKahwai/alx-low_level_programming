4238004ae6668a72b2d0742a7689fc2f5f571ae9
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-06-30 02:57:25 -0700

    concatenate 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,9 +16,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len, len1, len2, i;
 
 	if (s1 == NULL)
-		*s1 = "";
+		s1 = "";
 	if (s2 == NULL)
-		*s2 = "";
+		s2 = "";
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
@@ -34,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s3[i] = s1[i];
 	}
-	for (i = 0; i < len; i++)
+	for (i = 0; i < n; i++)
 	{
 		s3[len1 + i] = s2[i];
 	}
