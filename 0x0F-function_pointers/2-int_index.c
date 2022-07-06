c047888ffbba1d752e2683455861f67c88d6b9fc
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-07-06 05:28:29 -0700

    compare elements of an array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,18 +11,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	int flag = 0;
-
-	if (size <= 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array != NULL && cmp != NULL)
 	{
-		flag = cmp(array[i]);
-
-		if (flag != 0)
-			return (i);
-		if (i == (size - 1))
-			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
 	}
+
+	return (-1);
 }
