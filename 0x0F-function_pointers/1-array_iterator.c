2145af61c27d0c17b5dd70556051c5fe6f721173
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-07-06 03:29:32 -0700

    print elements of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include<stdlib.h>
+
+/**
+ * array_iterator - calls functions to print elements of an array
+ * @array: array whose elements to print
+ * @size: no. of elements to print
+ * @action: function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
