ae5f1d0ca3de27be7d3dbeff1254832eab1d4268
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-07-14 04:37:45 -0700

    find the number of elements in the linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * list_len - finds the number of elements in a linked list_t list
+ * @h:pointer to first element of the list
+ * Return: no. of elements in the list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t len;
+	list_t *curr;
+
+	len = 0;
+
+	for (curr = h; curr; curr = curr->next)
+	{
+		len++;
+	}
+
+	return (len);
+}
+
