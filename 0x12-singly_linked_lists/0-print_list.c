da9fe929e3aa454fb131347c64a46adee5afcb4d
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-07-14 04:13:28 -0700

    change format specifier for length

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,21 +2,20 @@
 #include<stdio.h>
 
 /**
- * print-list - prints all the elements of a list_t list
+ * print_list - prints all the elements of a list_t list
  * @h: pointer to first element of list
  * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
 	size_t i;
-	h =  head;
 
 	if (h->str == NULL)
 		printf("[0] (nil)");
 	else
 		for (i = 0; h; i++)
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 			h = h->next;
 		}
 	return (i);
