00838e6f2afec37f1f4ceb2e35ebcc0297f0b570
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-07-14 09:15:03 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to first element of list
+ */
+
+void free_list(list_t *head)
+{
+	list_t curr;
+
+	while (head != NULL)
+	{
+		curr = head;
+		head = head->next;
+		free(curr);
+	}
+}
