50ee65068f498e098e4f43f755a72b457543fc00
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-07-14 11:44:37 -0700

    print a statement before main executes

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "lists.h"
+
+/**
+ * before_main - prints a statement before main is executed
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
