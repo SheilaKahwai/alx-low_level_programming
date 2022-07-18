5fc9ec9de8ccb84396e7cc130d3867b2583d1b48
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-07-18 01:55:31 -0700

    count number of elements in list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - counts the number of elements in a linked listint_t list
+ * @h: pointer to first element in the list
+ * Return: number of nodes in the list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+	const listint_t *curr;
+
+	count = 0;
+
+	for (curr = head; curr; curr = curr->next)
+	{
+		count++;
+	}
+
+	return (count);
+}
