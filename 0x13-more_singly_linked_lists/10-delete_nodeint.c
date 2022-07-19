e0d12f5d8403c7601936c6cbcaa8896fa5762b6a
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-07-19 07:28:19 -0700

    change variable name

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp, *newnode;
+	listint_t *temp, *nextnode;
 	unsigned int i;
 
 	temp = *head;
@@ -20,8 +20,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
-		temp = (*head)->next;
-		free(*head);
+		*head = (*head)->next;
+		free(temp);
 		return (1);
 	}
 	while (i < (index - 1))
