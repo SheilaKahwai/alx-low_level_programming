bb2618ffda6ceb735d820af73fb4eeb9da04b49e
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-07-19 06:09:04 -0700

    add conditional statement

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	while (count < idx)
+	while ((count < idx) && curr)
 	{
 		if (count == (idx - 1))
 		{
