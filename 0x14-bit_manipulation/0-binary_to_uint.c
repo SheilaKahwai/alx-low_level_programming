e3c219ba517c5b5d04d08bd389eb47e1ea77d8a0
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-07-21 03:08:19 -0700

    convert binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,24 +9,16 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec_val = 0;
-	unsigned int base = 1;
-	unsigned int len = 0;
 	unsigned int i;
 
 	if (!b)
 		return (0);
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		len++;
-	}
 
-	for (i = (len - 1); b[i]; i--)
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] > '1' || b[i] < '0')
 			return (0);
-		if (b[i] == '1')
-			dec_val += base;
-		base = base * 2;
+		dec_val = dec_val * 2 + (b[i] - '0');
 	}
 	return (dec_val);
 }
