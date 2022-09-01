517254dcfb8c02a0d3a2aafaf6af69fcb99b5be7
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-09-01 01:43:54 -0700

    find the nth node

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - finds the node at specified index
+ * @head: pointer to first node of list
+ * @index: position of node to find
+ * Return: address of node or NULL if it does not exist
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+  unsigned int count = 0;
+  dlistint_t *curr = head;
+
+  if (!head)
+    return (NULL);
+  if (index == 0)
+    return (head);
+  while (count != index && curr->next)
+    {
+      curr = curr->next;
+      count++;
+    }
+  if (count == index)
+    return (curr);
+  return (NULL);
+}
