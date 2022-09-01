08b19d81f032764743870ae7eff4fa9d015f0abd
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-09-01 02:20:20 -0700

    change print to printf

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -14,7 +14,7 @@ size_t print_dlistint(const dlistint_t *h)
     return (count);
   while (h)
     {
-      print("%d\n", h->n);
+      printf("%d\n", h->n);
       h = h->next;
       count++;
     }
