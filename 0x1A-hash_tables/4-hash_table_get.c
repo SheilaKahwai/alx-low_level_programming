43bddc20bc552bb4e00a18ca1a70c2ba735b045e
Sheila Kahwai <SheilaKahwai@users.noreply.github.com>
2022-09-22 10:42:39 -0700

    task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get -  retrieves a value associated with a key
+ * @ht: pointer to hash table to look into
+ * @key: key to search for
+ * Return: the value associated with the element, or NULL
+ * if key couldn’t be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+unsigned long int idx;
+hash_node_t *curr;
+
+if (!ht || !key)
+return (NULL);
+  
+idx = key_index((unsigned char *)key, ht->size);
+curr = ht->array[idx];
+
+while (curr)
+{
+if (strcmp(curr->key, key) == 0)
+return (curr->value);
+curr = cuur->next;
+}
+return (NULL);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,5 +40,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 int check_key(hash_node_t *ht, const char *key);
 void update_value(hash_node_t **ht, const char *key, const char *value);
 hash_node_t *add_node(hash_node_t **ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
 #endif
